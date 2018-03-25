6dfb008cf3cd5cfb73beaf348a7f82a7d9ba2096
葉鎧瑋 <a0979597291@gmail.com>
2018-03-25 12:36:57 +0800

    Add files via upload

---
+++ component/SubmitBtn.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Snackbar from 'material-ui/Snackbar';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import RaisedButton from 'material-ui/RaisedButton';
+
+class SubmitBtn extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      submit_open: false,
+    }
+  }
+
+  render() {
+    return (
+      <div className="btbox">
+        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+          <RaisedButton
+            onClick={this.props.btnonClick}
+            label="Like"/>
+          <Snackbar
+            className="snackbar"
+            open={this.props.isopen}
+            message={this.props.message}
+            autoHideDuration={4000}
+            onRequestClose={this.handleRequestClose}/>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+export default SubmitBtn;
